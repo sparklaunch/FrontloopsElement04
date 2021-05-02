afddaa60f5e716f012e7c8d69a7bcb134a7dd2b3
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-05-02 13:48:24 +0900

    add launch screen

--- FrontloopsElement04/Controllers/MainViewController.swift
+++ FrontloopsElement04/Controllers/MainViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var mainContainer: UIView!
+    @IBOutlet weak var animatedText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.updateMainContainer()
     }
 
 }
@@ -19,5 +21,11 @@ class MainViewController: UIViewController {
 // MARK: - Initialization
 
 extension MainViewController {
-    
+    func updateMainContainer() {
+        self.mainContainer.layer.shadowColor = UIColor.darkGray.cgColor
+        self.mainContainer.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        self.mainContainer.layer.shadowRadius = 10.0
+        self.mainContainer.layer.shadowOpacity = 0.7
+        self.mainContainer.layer.cornerRadius = 10.0
+    }
 }
